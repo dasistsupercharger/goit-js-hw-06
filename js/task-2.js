48f521fddc710d17d0e87555a0d7ab63456df07d
dasistsupercharger <hardyhard833@gmail.com>
2024-01-05 19:58:14 +0200

    new hw

---
+++ js/task-2.js
@@ -0,0 +1,27 @@
+class Storage {
+  constructor(items) {
+    this._items = items;
+  }
+
+  getItems() {
+    return this._items;
+  }
+
+  addItem(newItem) {
+    this._items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const indexToRemove = this._items.indexOf(itemToRemove);
+    if (indexToRemove !== -1) {
+      this._items.splice(indexToRemove, 1);
+    }
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); 
+storage.addItem("Droid");
+console.log(storage.getItems()); 
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); 
