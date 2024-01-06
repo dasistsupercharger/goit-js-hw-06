9f7c6b9af4b1aec0d27eafb7cbda7afa8be17b2e
dasistsupercharger <hardyhard833@gmail.com>
2024-01-06 18:44:07 +0200

    upd

--- js/task-2.js
+++ js/task-2.js
@@ -1,27 +1,29 @@
 class Storage {
+  #items;
+
   constructor(items) {
-    this._items = items;
+    this.#items = items;
   }
 
   getItems() {
-    return this._items;
+    return this.#items;
   }
 
   addItem(newItem) {
-    this._items.push(newItem);
+    this.#items.push(newItem);
   }
 
   removeItem(itemToRemove) {
-    const indexToRemove = this._items.indexOf(itemToRemove);
+    const indexToRemove = this.#items.indexOf(itemToRemove);
     if (indexToRemove !== -1) {
-      this._items.splice(indexToRemove, 1);
+      this.#items.splice(indexToRemove, 1);
     }
   }
 }
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); 
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem("Droid");
-console.log(storage.getItems()); 
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 storage.removeItem("Prolonger");
-console.log(storage.getItems()); 
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
